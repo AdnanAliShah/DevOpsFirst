3eb65863e34dd1b756a59c0f96b1de9ecaa84f68
Adnan <shahadnanali6@gmail.com>
2023-08-22 17:48:17 +0500

    modified server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require ("express");
+
+const app = express();
+
+const port = 1652;
+
+app.get("/data" , (req, res) => {
+    res.send()
+});
+
+app.listen(port, (err, data) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(`http://localhost: ${port}`)
+    }
+});
